916190a75ad361df3739d79c7b03e276242a8fd6
Apoorva pratap singh <apoorvapratapsingh6@gmail.com>
2025-12-21 19:37:13 +0530

    Updated app file

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -19,7 +19,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://internship-lms-rpwo.vercel.app"
+      "https://internship-lms-klht.vercel.app"
     ],
     credentials: true
   })
